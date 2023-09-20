cd1e0aa9477c2cbb05704e237c8b55bf65adb1a4
fuzail <fuzail@code4each.com>
2023-09-20 12:05:03 +0530

    cors origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://code4each.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
