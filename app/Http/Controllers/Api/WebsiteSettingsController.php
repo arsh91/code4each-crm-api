8c34c4d25ac611bd0bc4663af61cdc48c88962a9
fuzail <fuzail@code4each.com>
2023-10-25 16:58:54 +0530

    changes in showing data

--- app/Http/Controllers/Api/WebsiteSettingsController.php
+++ app/Http/Controllers/Api/WebsiteSettingsController.php
@@ -15,8 +15,24 @@ class WebsiteSettingsController extends Controller
         if(!$website_id){
             return response()->json(["error" => "website id is required."],400);
         }
-        $websiteDetail = Websites::with('agencyWebsiteDetail')->where('id', $website_id)->select(['id', 'website_domain', 'assigned'])->first()->toArray();
-
+        $websiteData = Websites::with('agencyWebsiteDetail')->where('id', $website_id)->first();
+        $agencyWebsiteDetail = $websiteData->agencyWebsiteDetail;
+        $websiteDetail = [
+            "id" => $websiteData->id,
+            "website_domain" => $websiteData->website_domain,
+            "assigned" => $websiteData->assigned,
+            "agency_website_detail" =>[
+                "id" => $agencyWebsiteDetail->id,
+                "business_name" => $agencyWebsiteDetail->business_name,
+                "address" => $agencyWebsiteDetail->address,
+                "description" => $agencyWebsiteDetail->description,
+                "logo" => '/storage/'.$agencyWebsiteDetail->logo,
+                "agency_id" => $agencyWebsiteDetail->agency_id,
+                "website_id" => $agencyWebsiteDetail->website_id,
+                "status" => $agencyWebsiteDetail->status,
+                "created_by" => $agencyWebsiteDetail->created_by,
+            ],
+        ];
         if (!empty($websiteDetail)) {
             $response = [
                 'message' => "Detail Fetched Successfully.",
